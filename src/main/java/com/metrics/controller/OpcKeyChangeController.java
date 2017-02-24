98740ee04fb3f2660df47c0983ac34395d3f6867
ethan.lee <ethan.lee@fubon.com>
2017-02-24 10:23:42 +0800

    add controller

--- src/main/java/com/metrics/controller/OpcKeyChangeController.java
+++ src/main/java/com/metrics/controller/OpcKeyChangeController.java
@@ -3,6 +3,7 @@ package com.metrics.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.formosoft.ra.taica2.RAFacade2;
 import com.metrics.service.message.opc.OpcKeyChangeService;
 
-
+@Controller
 @RequestMapping("/message/OpcKeyChange")
 public class OpcKeyChangeController
 {
